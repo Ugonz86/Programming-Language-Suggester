c69df239334ec004de535b3faf228b12e4388a9c
Ugonz86 <ugonzalez86@gmail.com>
2019-10-25 13:01:28 -0700

    Add html, css and js essential functionality. The user can select options from a form and click submit to get a response based on js code.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,18 @@
-$(document).ready(function() {
+var which = $("input:radio[name=question1]:checked").val();
 
+$(document).ready(function() {
+  $("form#userForm").submit(function(event) {
+    event.preventDefault();
+    var why = $("input:radio[name=question1]:checked").val();
+    var what = $("input:radio[name=question2]:checked").val();
+    var which = $("input:radio[name=question3]:checked").val();
+
+    var result = "";
+    if (why === "developer" && what === "create" && which === "ios") {
+      result = "Swift";
+    }
+
+    $("#lang").text(result);
+    $("#result").show();
+  });
 });
