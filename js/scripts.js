2b214976deaf56b98f2a082902d57277255729b4
Ugonz86 <ugonzalez86@gmail.com>
2019-10-25 15:23:08 -0700

    add UI and js code to get more results from all the form options.

--- js/scripts.js
+++ js/scripts.js
@@ -8,9 +8,120 @@ $(document).ready(function() {
     var which = $("input:radio[name=question3]:checked").val();
 
     var result = "";
-    if (why === "developer" && what === "create" && which === "ios") {
-      result = "Swift";
+    if (why === "developer" && what === "web" && which === "manager") {
+      result = "Java, Python, Ruby and/or PHP are the most used web languages at a corporate level";
     }
+    if (why === "developer" && what === "web" && which === "developer") {
+      result = "HTML5 for web development and Swift, Java and C# for web development";
+    }
+    if (why === "developer" && what === "web" && which === "scientist") {
+      result = "R and Python are are the best languages to start in the analysis and big data areas.";
+    }
+
+
+
+    if (why === "developer" && what === "games" && which === "manager") {
+      result = "C# and C++ are the best languages for game designing and development.";
+    }
+    if (why === "developer" && what === "games" && which === "developer") {
+      result = "C# and C++ are the best languages for game designing and development.";
+    }
+    if (why === "developer" && what === "games" && which === "scientist") {
+      result = "C# and C++ are the best languages for game designing and development.";
+    }
+
+    if (why === "developer" && what === "data" && which === "manager") {
+      result = "C# and C++ are the best languages for game designing and development.";
+    }
+    if (why === "developer" && what === "data" && which === "developer") {
+      result = "C# and C++ are the best languages for game designing and development.";
+    }
+    if (why === "developer" && what === "data" && which === "scientist") {
+      result = "C# and C++ are the best languages for game designing and development.";
+    }
+
+
+
+    if (why === "skill" && what === "web" && which === "manager") {
+      result = "Java, Python, Ruby and/or PHP are the most used web languages at a corporate level";
+    }
+    if (why === "skill" && what === "web" && which === "developer") {
+      result = "HTML5 for web development and Swift, Java and C# for web development";
+    }
+    if (why === "skill" && what === "web" && which === "scientist") {
+      result = "R and Python are the best languages in this field.";
+    }
+
+
+    if (why === "skill" && what === "games" && which === "manager") {
+      result = "Python, C# and Ruby";
+    }
+    if (why === "skill" && what === "games" && which === "developer") {
+      result = "HTML5 and C#";
+    }
+    if (why === "skill" && what === "games" && which === "scientist") {
+      result = "HTML5 and C#";
+    }
+
+
+    if (why === "skill" && what === "data" && which === "manager") {
+      result = "R and Python are the ideal langueges for you.";
+    }
+    if (why === "skill" && what === "data" && which === "developer") {
+      result = "R and Python are the ideal langueges for you.";
+    }
+    if (why === "skill" && what === "data" && which === "scientist") {
+      result = "R and Python are the ideal langueges for you.";
+    }
+
+
+
+    if (why === "fun" && what === "web" && which === "manager") {
+      result = "Java, Python, Ruby and/or PHP are the most used web languages at a corporate level";
+    }
+    if (why === "fun" && what === "web" && which === "developer") {
+      result = "HTML5 for web development and Swift, Java and C# for web development";
+    }
+    if (why === "fun" && what === "web" && which === "scientist") {
+      result = "R and Python are are the best languages to start in the analysis and big data areas.";
+    }
+
+
+    //
+    // if (why === "skill" && what === "create" && which === "manager") {
+    //   result = "Java, Python, Ruby and/or PHP are the most used web languages at a corporate level";
+    // }
+    // if (why === "skill" && what === "create" && which === "developer") {
+    //   result = "HTML5 for web development and Swift, Java and C# for web development";
+    // }
+    // if (why === "skill" && what === "create" && which === "scientist") {
+    //   result = "Java";
+    // }
+    //
+    //
+    // if (why === "fun" && what === "create" && which === "manager") {
+    //   result = "Javascript, Python and Ruby;
+    // }
+    // if (why === "fun" && what === "create" && which === "developer") {
+    //   result = "For web developement HTML5 and JavaScript. For mobile development, HMTL5, Swift, Java and C#. For games, also HTML5, Python, and C#;
+    // }
+    // if (why === "fun" && what === "create" && which === "scientist") {
+    //   result = "Javascript, Python and Ruby";
+    // }
+    //
+    //
+    // if (why === "fun" && what === "create" && which === "manager") {
+    //   result = "Javascript, Python and Ruby;
+    // }
+    // if (why === "fun" && what === "create" && which === "developer") {
+    //   result = "For web developement HTML5 and JavaScript. For mobile development, HMTL5, Swift, Java and C#. For games, also HTML5, Python, and C#;
+    // }
+    // if (why === "fun" && what === "create" && which === "scientist") {
+    //   result = "Javascript, Python and Ruby";
+    // }
+
+
+
 
     $("#lang").text(result);
     $("#result").show();
